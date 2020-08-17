1bb8845a0da3e4dad546787427baf814b5e008ec
kazuki70102 <kazuki70102@gmail.com>
2020-08-17 16:20:52 +0900

    Update　views

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,5 @@
 class ContactsController < ApplicationController
+  layout "static_pages"
 
   def new
     @contact = Contact.new
@@ -31,15 +32,3 @@ class ContactsController < ApplicationController
                           :content)
     end
 end
-
-
-# def create
-#     @user = User.new(user_params)
-#     if @user.save
-#       @user.send_activation_email
-#       flash[:info] = "Please check your email to activate your account."
-#       redirect_to root_url
-#     else
-#       render "new"
-#     end
-#   end
