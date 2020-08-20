3d30167572804f00941c227d2def49eb960309a2
kazuki70102 <kazuki70102@gmail.com>
2020-08-20 17:35:09 +0900

    Update language devise_invitable to japanese

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
-    confirmations: 'users/confirmations'
+    confirmations: 'users/confirmations',
+    invitations: 'users/invitations' 
   }
 
   root "static_pages#home"
